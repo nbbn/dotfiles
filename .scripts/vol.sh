3d191e0af2af4b7e687cc76f39c5e2571cfbf1ba
Matthew <toshibamatt@gmail.com>
2013-04-27 22:00:04 +0100

    Modified vol.sh so if the volume = "[0%]" "[Mute]" is echoed instead.

--- .scripts/vol.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#Print current info about master volume channel, find "Mono", cut the output to what we need;
-vol=`amixer get Master | grep "Mono:" | grep -o '\[.*%\]'`
-#Echo our output;
-echo $vol
